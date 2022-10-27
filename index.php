017f4063341d280b95f8a51a2fce32fa24e14969
Nurul Umami <nurulumami1945@gmail.com>
2022-10-27 10:07:55 +0700

    belajar dasar php pertemuan 1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World! <br>";
+
+$nama = "Nurul Umami";
+$umur = "20";
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaKakak = "Imam Suyuti";
+$umurKakak = "28";
+
+echo "Nama kakak saya <strong>$namaKakak</strong>, kakak saya berusia $umurKakak tahun.<br>";
+
+$selisihUmur = $umurKakak - $umur;
+echo "Selisih usia saya dan kakak saya adalah $selisihUmur tahun.";
+
+?>
